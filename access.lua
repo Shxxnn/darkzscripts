99af250ea3556052b0d774d61e53afa6b608c1a9
Youssef <58625802+TeaXHub@users.noreply.github.com>
2022-12-24 01:21:16 +0200

    Create access.lua

---
+++ access.lua
@@ -0,0 +1,25 @@
+_G.Supported = {
+    [8750997647] = {
+        Game = "Tapping Legends X",
+        Path = "TappingLegendsX"
+    },
+    [9551640993] = {
+        Game = "Mining Simulator 2",
+        Path = "MiningSimulator2"
+    },
+}
+
+local function detectgame()
+   local UrlPath
+   for i,v in pairs(_G.Supported) do
+   if i == game.GameId then
+       UrlPath = v.Path
+    end
+end
+return UrlPath
+end
+
+if detectgame() ~= nil then loadstring(game:HttpGet("https://raw.githubusercontent.com/TeaXHub/DarkzHub/main/" .. detectgame() .. ".lua"))()
+else
+    return print('Game is not supported')
+end
